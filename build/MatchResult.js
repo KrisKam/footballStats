5c9a894c1121a28b442a7b35327b23c83097ed31
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-08-16 13:24:02 -0600

    convert values in array to appropriate type values

---
+++ build/MatchResult.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//enum is like an object that stores closely related values
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
---
+++ src/MatchResult.ts
@@ -0,0 +1,6 @@
+//enum is like an object that stores closely related values
+export enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
