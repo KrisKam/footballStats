06a9479f1e4814d584d499a28b369ff06f05bab2
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-08-17 15:27:31 -0600

    refactor using generics

---
+++ src/HoldAnything.ts
@@ -0,0 +1,12 @@
+class HoldAnything<TypeOfData> {
+  data: TypeOfData;
+}
+
+const holdNumber = new HoldAnything<number>();
+holdNumber.data = 123;
+
+const holdString = new HoldAnything<string>();
+holdString.data = "abcde";
+
+
+
